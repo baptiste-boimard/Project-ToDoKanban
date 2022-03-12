74b690d4686a3cb74bb4e7030094f82810b8227b
baptiste-boimard <bouketin27@gmail.com>
2022-03-12 18:46:47 +0100

    add adress heroku databse to db.js

--- app/db.js
+++ app/db.js
@@ -1,6 +1,9 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(process.env.PG_URL, {
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    ssl: {
+        rejectUnauthorized : false
+    },
     define: {
         createdAt: "created_at",
         updatedAt: "updated_at"

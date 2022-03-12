b0de3a50072abee53bf16e1b71180d9ada33607a
baptiste-boimard <bouketin27@gmail.com>
2022-03-12 18:56:59 +0100

    essai config database

--- app/db.js
+++ app/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
+const sequelize = new Sequelize(process.env.PG_URL, {
     ssl: {
         rejectUnauthorized : false
     },

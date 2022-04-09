05792165b813d3a98e03ed8bb5630582d78189f3
baptiste-boimard <bouketin27@gmail.com>
2022-04-09 19:15:27 +0200

    bundle

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const utilsModule = require('./utils');
 
 const app = {
     
-    baseUrl : 'https://project-todokanban.herokuapp.com/:3030/',
+    baseUrl : 'https://project-todokanban.herokuapp.com/',
     
     
     //TODO GROUPER DANS LE MEME PROJET QUE LE BACK
